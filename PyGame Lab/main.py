86ad23f949fedc9b196f10b521637c7fbf0abd7a
CharlesMakesGames <cfisher@unomaha.edu>
2021-11-10 22:04:21 -0600

    Timers and Endings
    Added a timer that auto flips the it status if nobody is touched in 10sec. Changed Sprites. Added Music. Added lives so the game can actually end without a force quit.

--- PyGame Lab/main.py
+++ PyGame Lab/main.py
@@ -3,16 +3,25 @@ import time
 import threading
 import pygame
 import os
-from random import randrange
-pygame.font.init()
 
+
+from random import randrange
+file = 'Assets/Attic Secrets.mp3'
+pygame.init()
+pygame.font.init()
+pygame.mixer.init()
+pygame.mixer.music.load(file)
+pygame.mixer.music.play()
+pygame.event.wait()
+
+HIT_SOUND = pygame.mixer.Sound('Assets/Grenade+1.mp3')
 
 # Create our Main Surface
 WIDTH, HEIGHT = 1200, 800  # Define Width and Height as a Tuple
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # Create the surface
 pygame.display.set_caption("Play Tag!")  # Label the window with game name
 
-HUD_FONT = pygame.font.SysFont('arial', 50)
+HUD_FONT = pygame.font.SysFont('arial', 40)
 
 # Color Pallette
 GRASS_GREEN = (50, 175, 60)
@@ -22,12 +31,12 @@ WHITE = (255, 255, 255)
 FPS = 60
 
 # Sprites
-SPRITE_WIDTH = 32
-SPRITE_HEIGHT = 32
-DEER_SPRITE = pygame.image.load(os.path.join('Assets', 'ham.png'))
+SPRITE_WIDTH = 128
+SPRITE_HEIGHT = 128
+DEER_SPRITE = pygame.image.load(os.path.join('Assets', 'deer.png'))
 DEER = pygame.transform.scale(DEER_SPRITE, (SPRITE_WIDTH, SPRITE_HEIGHT))
 
-WOLF_SPRITE = pygame.image.load(os.path.join('Assets', 'wolf.png'))
+WOLF_SPRITE = pygame.image.load(os.path.join('Assets', 'arctic-fox.png'))
 WOLF = pygame.transform.scale(WOLF_SPRITE, (SPRITE_WIDTH, SPRITE_HEIGHT))
 
 # RANDOM START POSITIONS
@@ -38,10 +47,11 @@ WOLF_STARTX = random.randint((WIDTH/2),(WIDTH-5))
 WOLF_STARTY = random.randint(5,HEIGHT)
 
 # PASSABLE IT STATUS?
+# starts randomly for players
 it_status = random.randint(1, 2)
 
+# to tag backsies timer countdown
 timer_thing = 0
-
 def update_timer():
     global timer_thing
     timer_thing = 5
@@ -49,7 +59,24 @@ def update_timer():
         time.sleep(1)
         timer_thing -= 1
 
+# tags remaining so the game can actually end
+wolftags = 5
+deertags = 5
 
+# lets get a shotclock kind of thing that flips the it status every 10sec
+tag_timer = 0
+def tag_clock():
+    global tag_timer
+    global it_status
+    tag_timer = 10
+    while tag_timer > 0:
+        time.sleep(1)
+        tag_timer -= 1
+    if get_it() == 1:
+        it_status = 2
+    elif get_it() == 2:
+        it_status = 1
+    tag_clock()
 
 def get_it():
     return it_status
@@ -71,7 +98,7 @@ CAUGHT = pygame.USEREVENT + 1  # the plus segment adds to the event queue
 # if adding more you use a higher number to the queue, +2 and soforth
 
 # Game Parameters
-SPEED = 5  # what gets added to the position of a sprite when key is pressed
+SPEED = 7  # what gets added to the position of a sprite when key is pressed
 
 
 # Define a main function that runs the game
@@ -81,23 +108,32 @@ def main():
     wolf = pygame.Rect(WOLF_STARTX, WOLF_STARTY, SPRITE_WIDTH, SPRITE_HEIGHT)
 
     clock = pygame.time.Clock() # this creates a clock that keeps track of run and framerate
+
+    t2 = threading.Thread(target=tag_clock)
+    t2.daemon = True
+    t2.start()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play()
+    pygame.event.wait()
     run = True  # set run to True
     # While loop that runs the game
     while run:  # Game Loop
 
         clock.tick(FPS) # uses the defined FPS value to tick the loop along
+        # start the shotclock timer
+
         for event in pygame.event.get():  # Checks for EVENTS
             if event.type == pygame.QUIT:  # if close clicked
                 run = False  # change run to False to break loop
+            if wolftags == 0:
+                draw_winner("Deer Wins!")
+                run = False
+            elif deertags == 0:
+                draw_winner("Wolf Wins!")
+                run = False
 
-            """
             if event.type == CAUGHT:
-                global it_status
-                if get_it() == 1:
-                    set_it(2)
-                elif get_it() == 2:
-                    set_it(1)
-            """
+               HIT_SOUND.play()
 
         keys_pressed = pygame.key.get_pressed()
         if keys_pressed[pygame.K_a] and deer.x - SPEED > 0:  # Deer Left...the and statement prevents leaving the edge of the screen
@@ -126,8 +162,6 @@ def main():
 # Draw Window Function
 def draw_window(deer, wolf):
     WIN.fill(GRASS_GREEN)  # Draw the Grass
-    WIN.blit(DEER, (deer.x, deer.y))  # Sprites
-    WIN.blit(WOLF, (wolf.x, wolf.y))  # Sprites
 
     # make sure this is BEFORE your update, 5 head
     it_status_display = HUD_FONT.render(
@@ -136,26 +170,57 @@ def draw_window(deer, wolf):
 
     timer_status = HUD_FONT.render(
         'No Tag Back: ' + str(timer_thing), 1, WHITE)
-    WIN.blit(timer_status, (WIDTH - timer_status.get_width() -10, 10))
+    WIN.blit(timer_status, (WIDTH - timer_status.get_width() - 10, 10))
+
+    wolftag_hud = HUD_FONT.render(
+        'Wolf Lives: ' + str(wolftags), 1, WHITE)
+    WIN.blit(wolftag_hud, (WIDTH - wolftag_hud.get_width() - 10, 700))
+
+    deertag_hud = HUD_FONT.render(
+        'Deer Lives: ' + str(deertags), 1, WHITE)
+    WIN.blit(deertag_hud, (10, 700))
+
+    tagtimer_hud = HUD_FONT.render(
+        'Auto-It In: ' + str(tag_timer), 1, WHITE)
+    WIN.blit(tagtimer_hud, (WIDTH/2 - tagtimer_hud.get_width() / 2, 650))
+
+    WIN.blit(DEER, (deer.x, deer.y))  # Sprites
+    WIN.blit(WOLF, (wolf.x, wolf.y))  # Sprites
 
     pygame.display.update()  # Update the screen
 
-
-
+# When one player is out of lives, display that the other has won.
+def draw_winner(text):
+    draw_text = HUD_FONT.render(text, 1, WHITE)
+    WIN.blit(draw_text, (WIDTH / 2 - draw_text.get_width() /
+                         2, HEIGHT / 2 - draw_text.get_height() / 2))
+    pygame.display.update()
+    pygame.time.delay(5000)
 
 
 # Create a function to determine if tagged
 def deer_tagged(deer, wolf):
     if deer.colliderect(wolf):
-        # pygame.event.post(pygame.event.Event(CAUGHT))
+
         global it_status
+        global deertags
+        global wolftags
+        global tag_timer
+
         if timer_thing == 0:
+            pygame.event.post(pygame.event.Event(CAUGHT))
+            tag_timer = 10
             if get_it() == 1:
                 it_status = 2
+                deertags -= 1
             elif get_it() == 2:
                 it_status = 1
+                wolftags -= 1
+            # start the no-backsies countdown
             t = threading.Thread(target=update_timer)
+            t.daemon = True
             t.start()
+
 
 
 if __name__ == "__main__":
